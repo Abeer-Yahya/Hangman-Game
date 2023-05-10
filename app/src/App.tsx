e38a28cbab207391b34a565284ece62fedaa21ee
Abeer-Yahya <abeer.yahya799@gmail.com>
2023-05-10 10:15:33 +0300

    Initial commit

---
+++ app/src/App.tsx
@@ -0,0 +1,15 @@
+import landingImage from "./assets/hangman.png";
+import "./App.css";
+
+function App() {
+  return (
+    <>
+      <div>
+        <img src={landingImage} className="logo react" alt="React logo" />
+      </div>
+      <h1>Vite + React</h1>
+    </>
+  );
+}
+
+export default App;
